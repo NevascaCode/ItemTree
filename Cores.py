e515184cd81dee4ed983401d7d89923cad91c39f
Kawan Henrique <kawan.inf@gmail.com>
2021-01-07 21:09:10 -0300

    Add mais cores

--- Cores.py
+++ Cores.py
@@ -1,6 +1,15 @@
 class Cores(object):
-    def __init__(self, text):
-        self.text = text
+    negrito = '\033[1m'
 
-    def red(self):
-        print(f"\033[31m{self.text}\033[0m")
+    preto = '\033[30m'
+    vermelho = '\033[31m'
+    verde = '\033[32m'
+    amarelo = '\033[33m'
+    azul = '\033[34m'
+    rosa = '\033[35m'
+    ciano = '\033[36m'
+    branco = '\033[37m'
+
+    limpa = '\033[0m'
+    def limpador():
+        print('\033[0m', end='')
